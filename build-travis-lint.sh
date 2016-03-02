603bfff506610a19c8cea776baaff5e49b5dec7a
Mike Kobit <mkobit@gmail.com>
2016-03-02 10:05:55 -0600

    Delete the old travis 'lint' wrapper scripts

--- build-travis-lint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -x
-docker build -f Dockerfile-travis-lint -t travis:lint .
--- exec-travis-lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e -x
-LINT_ARGS="--no-interactive"
-if [[ "$#" -gt 0 ]]; then
-  LINT_ARGS="$@"
-fi
-docker run -it --rm -v $(pwd):/tmp/travis-workdir travis:lint ${LINT_ARGS}
